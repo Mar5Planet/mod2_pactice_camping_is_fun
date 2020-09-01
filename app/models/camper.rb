8abdb2bb937af422bcaf1d32a73e7cbdce3ba1f9
Mar5Planet <amustaf@email.arizona.edu>
2020-09-01 11:14:56 -0400

    all delieverables met

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,2 +1,8 @@
 class Camper < ApplicationRecord
+    has_many :signup
+    has_many :activities, through: :signup
+
+    validates :name, uniqueness: true
+    validates :age, numericality: {less_than_or_equal_to:  18, greater_than_or_equal_to: 8 }
 end
+
---
+++ app/models/signup.rb
@@ -0,0 +1,6 @@
+class Signup < ApplicationRecord
+    belongs_to :camper
+    belongs_to :activity
+
+    validates :time, numericality: { less_than_or_equal_to:  23, greater_than_or_equal_to: 0 }
+end 
